129a5e7dddc2593b7dc4636114ad6f6b1beb7703
kireeti <rayaprolu.kireeti@gmail.com>
2022-06-06 21:38:07 -0400

    Basic Template

--- TODO_Django/main/urls.py
+++ TODO_Django/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns=[
-    path("<str:name>",views.index,name="index"),
+    path("<str:name>",views.index,name="name"),
+    path("",views.home,name="home")
     
 ]
